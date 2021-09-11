745ca6603450e01cdab7379bf7afb1c193fdc834
unknown <leonardoalmeida2703@gmail.com>
2021-09-10 21:50:00 -0300

    Implementação do Security

--- Blog/Blog/src/main/java/com/BlogPessoalLeo/Blog/security/UserDetailsImplements.java
+++ Blog/Blog/src/main/java/com/BlogPessoalLeo/Blog/security/UserDetailsImplements.java
@@ -1,5 +1,59 @@
 package com.BlogPessoalLeo.Blog.security;
 
-public class UserDetailsImplements {
+import java.util.Collection;
+import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.BlogPessoalLeo.Blog.model.UsuarioModel;
+
+public class UserDetailsImplements implements UserDetails{
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	private String senha;
+	private List<GrantedAuthority> autorizacoes;
+	
+	
+	public UserDetailsImplements(UsuarioModel usuario) {
+		this.email = usuario.getEmail();
+		this.senha = usuario.getSenha();
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return autorizacoes;		
+	}
+	
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+	
+	@Override
+	public String getUsername() {
+		return email;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- Blog/Blog/src/main/java/com/BlogPessoalLeo/Blog/security/UserDetailsServiceImplements.java
+++ Blog/Blog/src/main/java/com/BlogPessoalLeo/Blog/security/UserDetailsServiceImplements.java
@@ -1,5 +1,30 @@
 package com.BlogPessoalLeo.Blog.security;
 
-public class UserDetailsServiceImplements {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.BlogPessoalLeo.Blog.model.UsuarioModel;
+import com.BlogPessoalLeo.Blog.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImplements implements UserDetailsService{
+	
+	private @Autowired UsuarioRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<UsuarioModel> objetoOptional = repository.findByEmail(username);
+		
+		if (objetoOptional.isPresent()) {
+			return new UserDetailsImplements(objetoOptional.get());
+		} else {
+			throw new UsernameNotFoundException(username + "Não Existe!");
+		}
+	}
 
 }
