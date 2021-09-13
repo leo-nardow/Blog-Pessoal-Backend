2f1048340cb5a3d09163d933a1f9b028cfa2c467
unknown <leonardoalmeida2703@gmail.com>
2021-09-13 10:14:53 -0300

    [Feature Leo - Task: Add package configuration and class SwaggerConfig]

---
+++ Blog/Blog/src/main/java/com/BlogPessoalLeo/Blog/configuration/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.BlogPessoalLeo.Blog.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public Docket api() {
+		return null;
+			
+	}
+}
